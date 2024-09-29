97608dfe83949652f0d975e8b4f2c3c2bd8a6747
Robin Toor <robin.toor@health.telstra.com>
2024-09-29 17:28:27 +1000

    Added a few tests

--- src/test/java/stepDefinitions/stepDefinitions.java
+++ src/test/java/stepDefinitions/stepDefinitions.java
@@ -18,8 +18,10 @@ public class stepDefinitions {
 	
 	Faker faker = new Faker();
 	user userPayload = new user();
+	user updateUserPayloadUser = new user();
 	Response response;
 	scenarioContext context = new scenarioContext();
+	public static String filePath = "src/test/resources/TempData/userPayload.json";
 	
 	
 	@Given("The create new user payload is created with dummy data")
@@ -32,36 +34,69 @@ public class stepDefinitions {
 		userPayload.setEmail(faker.internet().safeEmailAddress());
 		userPayload.setPassword(faker.internet().password(7, 10, false));
 		userPayload.setPhone(faker.phoneNumber().cellPhone());
-		context.writeDataToFile(userPayload, "src/test/resources/TempData/userPayload.json");
-		
+		context.writeDataToFile(userPayload, filePath);		
 	}
+	
 	@When("The user trigger Api call {string} with {string} http request")
-	public void the_user_trigger_api_call_with_http_request(String string, String string2) {
-		
-		response = userEndpoints.createUser(userPayload);
+	public void the_user_trigger_api_call_with_http_request(String callType, String requestType) {
+		callType = callType.toLowerCase();
+		if(callType.contains("create new user")) {
+			response = userEndpoints.createUser(userPayload);
+		}
+		else if (callType.contains("update user")) {
+			response = userEndpoints.updateUser(updateUserPayloadUser.getUsername(), updateUserPayloadUser);
+		}	
+		else if (callType.contains("delete user")) {
+			response = userEndpoints.deleteUser(updateUserPayloadUser.getUsername());
+		}
     	response.then().log().all();
 	}
+	
 	@Then("The Api call should be succeeded with status code {int}")
 	public void the_api_call_should_be_succeeded_with_status_code(Integer int1) {
     	assertEquals(response.getStatusCode(), 200);
 	}
+	
 	@Then("{string} in response body should be {string}")
-	public void in_response_body_should_be(String string, String string2) {
-	    // Write code here that turns the phrase above into concrete actions
-		response.then().assertThat().body("type", equalTo("unknown"));
-		user pUser = context.readDataFromFile(user.class, "src/test/resources/TempData/userPayload.json");
-		System.out.print(pUser.getLastName());
+	public void in_response_body_should_be(String key, String value) {
+	    
+		//if value is username  then get its value from json data
+		if(value.toLowerCase().contains("username")) {
+	    	value = updateUserPayloadUser.getUsername(); 
+	    }
+		response.then().assertThat().body(key, equalTo(value));
 	}
 	
 	@Given("The details of the previous user are noted")
 	public void the_details_of_the_previous_user_are_noted() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		updateUserPayloadUser = context.readDataFromFile(user.class, filePath);
+		System.out.print(updateUserPayloadUser.getLastName());
 	}
-	@Given("Update <lastname> and <email>")
-	public void update_lastname_and_email() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+
+	@Given("Update {string} and {string} in the request body")
+	public void update_and(String input1, String input2) {
+		updateUserPayloadUser.setLastName(input1);
+		updateUserPayloadUser.setEmail(input2);
+		System.out.print(updateUserPayloadUser.getLastName());
+	    System.out.print(updateUserPayloadUser.getEmail());
 	}
+
+	@Then("{string} and {string} should be updated")
+	public void and_should_be_updated(String value1, String value2) {
+	    response = userEndpoints.readUser(updateUserPayloadUser.getUsername());
+	    response.then().assertThat().body("lastName", equalTo(value1));
+	    response.then().assertThat().body("email", equalTo(value2));  
+	    System.out.print(updateUserPayloadUser.getLastName());
+	    System.out.print(updateUserPayloadUser.getEmail());
+	}
+	
+	@Then("User record should be deleted")
+	public void user_record_should_be_deleted() {
+		response = userEndpoints.readUser(updateUserPayloadUser.getUsername());
+		assertEquals(response.getStatusCode(), 404);
+		System.out.print(response.getStatusCode());
+		response.then().assertThat().body("message", equalTo("User not found"));
+	}
+
 
 }
--- src/test/resources/features/petstoreUsers.feature
+++ src/test/resources/features/petstoreUsers.feature
@@ -5,9 +5,24 @@ Feature: Operations about users
     Given The create new user payload is created with dummy data
     When The user trigger Api call "Create new user" with "Post" http request
     Then The Api call should be succeeded with status code 200
-    And "status" in response body should be "OK"
+    And "type" in response body should be "unknown"
 
-  Scenario: Update user details
+  Scenario Outline: Update user details
     Given The details of the previous user are noted
-    And Update <lastname> and <email>
-    When The user trigger Api call "Create new user" with "Post" http request
+    And Update <lastname> and <email> in the request body
+    When The user trigger Api call "Update user" with "Put" http request
+    Then The Api call should be succeeded with status code 200
+    And "type" in response body should be "unknown"
+    And <lastname> and <email> should be updated
+
+    Examples: 
+      | lastname | email             |
+      | "Smith"  | "smith@email.com" |
+
+  Scenario: Delete user
+    Given The details of the previous user are noted
+    When The user trigger Api call "Delete user" with "Delete" http request
+    Then The Api call should be succeeded with status code 200
+    And "type" in response body should be "unknown"
+    And "message" in response body should be "username"
+    And User record should be deleted
